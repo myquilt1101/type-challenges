568c7d97be95a26efb715eab54dc0bdb612ce103
myquilt1101 <myquilt@126.com>
2022-07-16 13:02:34 +0800

    solve: 4-myPick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,9 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [key in K]: T[key]
+}
+// interface Todo {
+//   title: string
+//   description: string
+//   completed: boolean
+// }
+// type a = MyPick<Todo, 'title'>
